c547fdb541c6976fc27ab2a40bc07fbe62230c8b
Dini Yusriah <diniyusriah71105@gmail.com>
2026-06-21 10:09:46 +0700

    Mengubah program menjadi Hello World

--- DiniYusriah_P13.java
+++ DiniYusriah_P13.java
@@ -1,11 +1,5 @@
-// Biodata
 public class DiniYusriah_P13 {
     public static void main(String[] args) {
-
-        System.out.println("Nama : Dini Yusriah");
-        System.out.println("NIM : 241011403194");
-        System.out.println("Kelas : 04TPLP008");
-        System.out.println("Jurusan : Teknik Informatika");
-
+        System.out.println("Hello World");
     }
 }
